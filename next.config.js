68348fab612bbf0ee90d113fd1e4239274412318
iLevyTate <78818969+iLevyTate@users.noreply.github.com>
2024-09-23 12:48:17 -0400

    chore: Modify Next.js configuration

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  webpack: (config, { isServer }) => {
+    // This will ignore the problematic module in both dev and build
+    config.externals = [...(config.externals || []), '@mapbox/node-pre-gyp'];
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false,
+        path: false,
+        os: false,
+        // ...other fallbacks if needed
+      };
+    }
+    return config;
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
